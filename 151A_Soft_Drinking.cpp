935a5ef998f430969882a2a0f351bcdb93a4372c
dasshoumik95-ai <dasshoumik95@gmail.com>
2026-01-14 11:26:59 +0530

    Accepted: 151A - Soft Drinking

---
+++ 151A_Soft_Drinking.cpp
@@ -0,0 +1,19 @@
+// Codeforces Problem: 151A - Soft Drinking
+// Language: C++17 (GCC 7-32)
+
+#include <iostream>
+#include<algorithm>
+using namespace std;
+ int main()
+{
+    int n, k, l, c, d, p, nl, np;
+    cin>>n >>k >>l >> c>> d>>p >>nl >>np;
+        // Calculates the minimum (bottleneck) servings per person
+    int minnum = min({
+        (k*l/(n*nl)), // Servings limited by drink volume
+        (c*d/(n)),    // Servings limited by lime slices
+        (p/(n*np))    // Servings limited by salt volume
+    });
+        cout <<endl<<minnum<<endl;
+        return 0;
+}
