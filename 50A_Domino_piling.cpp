7cf13321f285c3fc55a908f1e249116177c7106a
dasshoumik95-ai <dasshoumik95@gmail.com>
2026-01-18 02:28:25 +0530

    Accepted: 50A - Domino piling

---
+++ 50A_Domino_piling.cpp
@@ -0,0 +1,10 @@
+// Codeforces Problem: 50A - Domino piling
+// Language: C++23 (GCC 14-64, msys2)
+
+#include<iostream>
+using namespace std;
+ int main(){
+    int m,n;
+    cin >>m>>n;
+    cout<<(m*n)/2;
+}
