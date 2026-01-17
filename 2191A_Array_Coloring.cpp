66d5733a8fdfdc207ab6f96e20eecc290f69304d
dasshoumik95-ai <dasshoumik95@gmail.com>
2026-01-17 14:53:53 +0530

    Accepted: 2191A - Array Coloring

---
+++ 2191A_Array_Coloring.cpp
@@ -0,0 +1,33 @@
+// Codeforces Problem: 2191A - Array Coloring
+// Language: C++17 (GCC 7-32)
+
+#include<bits/stdc++.h>
+using namespace std;
+ int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n; cin >> n;
+        int x=0;
+        int a[n];
+        for(int i=0;i<n;i++)
+        {
+            cin >> a[i];
+        }
+        for(int i=0;i<n-1;i++)
+        {
+            if(((a[i]%2==0)&&(a[i+1]%2==0))||(((a[i]%2)!=0)&&((a[i+1]%2)!=0))){
+                x=1;
+                break;
+            }
+        }
+        if(x==1)
+        cout << "NO" << endl;
+        else
+        cout << "YES" << endl;
+                }
+}
