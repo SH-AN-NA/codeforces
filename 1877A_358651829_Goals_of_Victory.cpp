98f1a55b73d8a62da1bd0a4b5633777f17bbdda5
dasshoumik95-ai <dasshoumik95@gmail.com>
2026-01-18 18:59:18 +0530

    Accepted: 1877A - Goals of Victory (Submission 358651829)

---
+++ 1877A_358651829_Goals_of_Victory.cpp
@@ -0,0 +1,23 @@
+// Codeforces Problem: 1877A - Goals of Victory
+// Submission ID: 358651829
+// Language: C++23 (GCC 14-64, msys2)
+
+#include<bits/stdc++.h> 
+using namespace std; 
+ int main()
+{ 
+    ios::sync_with_stdio(false); 
+    cin.tie(nullptr);
+     int t; cin >> t;
+    while(t--)
+    {
+        int n; cin >> n;
+        int x,sum=0;
+        for(int i =0; i<n-1 ; i++)
+        {
+            cin >> x;
+            sum +=x;
+        }
+        cout << (-1)*sum << endl;
+     }
+}
