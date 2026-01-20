4f34a8ba061506e4a6b2d06a36aa8edcdee6759a
dasshoumik95-ai <dasshoumik95@gmail.com>
2026-01-20 18:42:02 +0530

    Accepted: 263A - Beautiful Matrix (Submission 358975811)

---
+++ 263A_358975811_Beautiful_Matrix.cpp
@@ -0,0 +1,25 @@
+// Codeforces Problem: 263A - Beautiful Matrix
+// Submission ID: 358975811
+// Language: C++23 (GCC 14-64, msys2)
+
+#include<bits/stdc++.h>
+using namespace std;
+ int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+        int a[5][5];
+    int marki=0,markj=0;
+    for(int i =0;i<5 ;i++ )
+    {
+        for(int j=0; j<5;j++)
+        {
+            cin >> a[i][j];
+            if(a[i][j]==1)
+            {
+                marki=i ; markj=j;
+            }
+                    }
+            }
+    cout << abs(marki-2) + abs(markj-2)<< endl;
+}
