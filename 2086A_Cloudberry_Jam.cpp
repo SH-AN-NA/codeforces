928a62008fef5c29b6d62309135d5fbe27671f4c
dasshoumik95-ai <dasshoumik95@gmail.com>
2026-01-20 18:24:50 +0530

    Accepted: 2086A - Cloudberry Jam

---
+++ 2086A_Cloudberry_Jam.cpp
@@ -0,0 +1,14 @@
+// Codeforces Problem: 2086A - Cloudberry Jam
+// Language: C++23 (GCC 14-64, msys2)
+
+#include<iostream>
+using namespace std;
+ int main()
+{
+    int t; cin >> t;
+    while(t--)
+    {
+        int n ; cin >> n;
+        cout << 2*n << endl;
+    }
+}
