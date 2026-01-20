e952e55f14a767ec57bd9a630196497312b37875
dasshoumik95-ai <dasshoumik95@gmail.com>
2026-01-20 18:24:50 +0530

    Accepted: 2086A - Cloudberry Jam (Submission 358973377)

---
+++ 2086A_358973377_Cloudberry_Jam.cpp
@@ -0,0 +1,15 @@
+// Codeforces Problem: 2086A - Cloudberry Jam
+// Submission ID: 358973377
+// Language: C++23 (GCC 14-64, msys2)
+
+#include<iostream>
+using namespace std;
+ int main()
+{
+    int t; cin >> t;
+    while(t--)
+    {
+        int n ; cin >> n;
+        cout << 2*n << endl;
+    }
+}
