4c1b0bf0af0c38eead477c920c203b911f06b8b6
dasshoumik95-ai <dasshoumik95@gmail.com>
2026-01-23 18:37:00 +0530

    Accepted: 2166B - Tab Closing

---
+++ 2166B_Tab_Closing.cpp
@@ -0,0 +1,26 @@
+// Codeforces Problem: 2166B - Tab Closing
+// Language: C++23 (GCC 14-64, msys2)
+
+#include <bits/stdc++.h>
+using namespace std;
+ int func(int a, int b, int c)
+{
+     if ((b <= (double)a/c )|| b>=a)
+        return 1;
+     else
+    return 2;
+}
+ int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+     int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, n;
+        cin >> a >> b >> n;
+        cout << func(a, b, n) << endl;
+    }
+    return 0;
+}
