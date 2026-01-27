d2fd70978c397feda8c366513fae795161d14fdb
dasshoumik95-ai <dasshoumik95@gmail.com>
2026-01-27 20:13:38 +0530

    Accepted: 1858A - Buttons (Submission 360220592)

---
+++ 1858A_360220592_Buttons.cpp
@@ -0,0 +1,27 @@
+// Codeforces Problem: 1858A - Buttons
+// Submission ID: 360220592
+// Language: C++23 (GCC 14-64, msys2)
+
+#include <bits/stdc++.h>
+using namespace std;
+ int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+     int t;
+    cin >> t;
+     while(t--)
+    {
+        int a , b , c;
+        cin >> a >> b >> c ;
+         if(a> b)
+            cout << "First" << endl;
+        else if(a < b)
+            cout << "Second" << endl;
+                    else if (c%2==0)
+            cout << "Second" << endl;
+        else
+            cout << "First" << endl;
+     }
+    return 0;
+}
