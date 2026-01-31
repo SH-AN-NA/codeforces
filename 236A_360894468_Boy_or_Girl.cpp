c962f9d0d745a0a371d6a08fb53cbae85fe0b199
dasshoumik95-ai <dasshoumik95@gmail.com>
2026-01-31 16:49:28 +0530

    Accepted: 236A - Boy or Girl (Submission 360894468)

---
+++ 236A_360894468_Boy_or_Girl.cpp
@@ -0,0 +1,18 @@
+// Codeforces Problem: 236A - Boy or Girl
+// Submission ID: 360894468
+// Language: C++23 (GCC 14-64, msys2)
+
+#include<bits/stdc++.h>
+using namespace std;
+ int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+        string s;
+    cin >> s;
+    set <char> s1(s.begin(),s.end());
+    if (s1.size()%2==0)
+    cout <<  "CHAT WITH HER!";
+    else
+    cout <<   "IGNORE HIM!";
+      }
