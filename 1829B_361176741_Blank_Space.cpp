a023d35c0edd128e1e1f8b71c5e33b33ecced082
dasshoumik95-ai <dasshoumik95@gmail.com>
2026-02-02 17:12:38 +0530

    Accepted: 1829B - Blank Space (Submission 361176741)

---
+++ 1829B_361176741_Blank_Space.cpp
@@ -0,0 +1,30 @@
+// Codeforces Problem: 1829B - Blank Space
+// Submission ID: 361176741
+// Language: C++23 (GCC 14-64, msys2)
+
+#include<bits/stdc++.h>
+using namespace std;
+ int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+     int t; 
+    cin >> t;
+     while(t--)
+    {
+        int cnt0 = 0 , len = 0;
+        int n; cin >> n;
+        vector<int> a(n);
+        for( int &i : a )
+        {
+            cin >> i;
+            if(i==1)
+            {
+                cnt0=0;
+            }
+            else cnt0++;
+            len = max(len,cnt0);
+        }
+        cout << len << endl;
+    }
+}
