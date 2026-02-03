36f30ca5a63c39fad3ed6c47d6d4e591b3407caf
dasshoumik95-ai <dasshoumik95@gmail.com>
2026-02-03 19:33:10 +0530

    Accepted: 1A - Theatre Square

---
+++ 1A_Theatre_Square.cpp
@@ -0,0 +1,13 @@
+// Codeforces Problem: 1A - Theatre Square
+// Language: C++23 (GCC 14-64, msys2)
+
+#include <iostream>
+using namespace std;
+ int main()
+{
+    long long n, m, a;
+    cin >> n >> m >> a;
+     long long N = (n + a - 1) / a;
+    long long M = (m + a - 1) / a;  
+     cout << N * M;
+}
