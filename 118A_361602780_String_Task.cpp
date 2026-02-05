08ee3232d1379a9894c943e6447bc4556e7e6a86
dasshoumik95-ai <dasshoumik95@gmail.com>
2026-02-05 18:29:00 +0530

    Accepted: 118A - String Task (Submission 361602780)

---
+++ 118A_361602780_String_Task.cpp
@@ -0,0 +1,20 @@
+// Codeforces Problem: 118A - String Task
+// Submission ID: 361602780
+// Language: C++23 (GCC 14-64, msys2)
+
+#include<bits/stdc++.h>
+using namespace std;
+ int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+     string s;
+    cin >> s;
+         for(auto i : s)
+    {
+        if(i<97)
+            i+=32;
+        if((i!='a')&&(i!='e')&&(i!='i')&&(i!='o')&&(i!='u')&&(i!='y'))
+        cout << "." << i ;
+             }
+}
