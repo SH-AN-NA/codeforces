dbd1394d158e26404fe8d5d302e41b8b8ad3b67a
dasshoumik95-ai <dasshoumik95@gmail.com>
2026-03-10 13:21:05 +0530

    Accepted: 2179A - Blackslex and Password (Submission 366115353)

---
+++ 2179A_366115353_Blackslex_and_Password.cpp
@@ -0,0 +1,18 @@
+// Codeforces Problem: 2179A - Blackslex and Password
+// Submission ID: 366115353
+// Language: C++23 (GCC 14-64, msys2)
+
+#include<bits/stdc++.h>
+using namespace std;
+ int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+     int t ; cin >> t ;
+    while(t--)
+    {        
+        int k , x ;
+        cin >> k >> x ;
+                cout << k * x + 1 << endl ;
+                          }
+}
