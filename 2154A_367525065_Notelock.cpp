8d49b26ee07e2be9acc8a4447e091144a71dd9b9
dasshoumik95-ai <dasshoumik95@gmail.com>
2026-03-20 20:23:23 +0530

    Accepted: 2154A - Notelock (Submission 367525065)

---
+++ 2154A_367525065_Notelock.cpp
@@ -0,0 +1,42 @@
+// Codeforces Problem: 2154A - Notelock
+// Submission ID: 367525065
+// Language: C++23 (GCC 14-64, msys2)
+
+#include<bits/stdc++.h>
+using namespace std;
+ void solve(int n , int k)
+{
+    string s;
+        cin >> s;
+         int ans = 0;
+        int key = 0 ;
+         for(int i = 0; i < n; i++)
+        {
+            if(s[i] == '1' && key == 0)
+            {
+                key = k - 1 ;
+                ans ++ ;
+            }
+            if(s[i] == '1' && key != 0)
+            {
+                key = k - 1 ;
+            }
+            else if(s[i] != '1' && key != 0)
+            {
+                key--;
+            }
+        }
+         cout << ans << "\n";
+}
+  int main()
+{    
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+     int t; cin >> t;
+    while(t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        solve(n,k) ;
+    }
+}
