c5db425a0ef0d6429a7c9836b8d39a522807bcf3
dasshoumik95-ai <dasshoumik95@gmail.com>
2026-05-24 17:21:44 +0530

    Accepted: 1537B - Bad Boy (Submission 375964170)

---
+++ 1537B_375964170_Bad_Boy.cpp
@@ -0,0 +1,31 @@
+// Codeforces Problem: 1537B - Bad Boy
+// Submission ID: 375964170
+// Language: C++23 (GCC 14-64, msys2)
+
+#include <bits/stdc++.h>
+using namespace std;
+ //    ================== ⚡          ⚡ ==================
+// Author: Shoumik 25CS10034
+// Contest: Codeforces Round
+// Problem: Problem Name
+// =============================================================
+ #define fast ios::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+ const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+ void solve() {
+    ll n , m , i , j ;
+    cin >> n >> m >> i >> j ;
+        cout << 1 << " " << 1 << " " << n << ' ' << m <<'\n' ;
+}
+ int main() {
+    fast;
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
+ // ij= 11
+// maximum distance = corner n-1 m-1 return n-1 m-1
