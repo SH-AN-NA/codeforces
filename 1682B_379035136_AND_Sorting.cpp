cb3481cf5e8d75be08303da83d6bef74ba9edb4e
dasshoumik95-ai <dasshoumik95@gmail.com>
2026-06-15 16:43:51 +0530

    Accepted: 1682B - AND Sorting (Submission 379035136)

---
+++ 1682B_379035136_AND_Sorting.cpp
@@ -0,0 +1,62 @@
+// Codeforces Problem: 1682B - AND Sorting
+// Submission ID: 379035136
+// Language: C++23 (GCC 14-64, msys2)
+
+#include <bits/stdc++.h>
+using namespace std;
+ //    ================== ⚡          ⚡ ==================
+// Author: Shoumik 25CS10034
+// Contest: Contest Name
+// Problem: B_AND_Sorting
+// Rating: Rating
+// =============================================================
+ #define fast ios::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+ // Loops (0-n), (n-0), (1-n), (n-1)
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define for0(i, n) for(int i = 0; i < n; ++i)          // 0 to n-1
+#define forn(i, n) for(int i = n; i >= 0; --i)         // n down to 0
+#define for1(i, n) for(int i = 1; i <= n; ++i)         // 1 to n
+#define for1n(i, n) for(int i = n; i >= 1; --i)        // n down to 1
+ // Vector Helpers
+#define vecInput(v) for(auto &i : v) cin >> i;
+#define vecOutput(v) for(const auto &i : v) cout << i << ' '; cout << '\n';
+ // Fast I/O Helpers
+#define YES cout << "YES" << '\n';
+#define NO cout << "NO" << '\n';
+ // Debugger (Runs locally with -DLOCAL flag)
+#ifdef LOCAL
+#define deb(x) cerr << #x << " = " << x << endl;
+#else
+#define deb(x)
+#endif
+ const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+ // Safe Modular Arithmetic Helpers
+ll modAdd(ll a, ll b) { return ((a % MOD) + (b % MOD)) % MOD; }
+ll modMul(ll a, ll b) { return ((a % MOD) * (b % MOD)) % MOD; }
+ void solve() {
+    int n ; cin >> n ;
+    vector<int> a(n) ;
+    int andd = (1<<30)-1 ;
+    for0(i,n)
+    {
+        cin >> a[i] ;
+        if(a[i]!=i) andd &= a[i] ;
+     }
+    cout << andd << '\n' ;
+}
+ int main() {
+    fast;
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
