edff5058b5aa8616d1b6e6b4b031220942ec3958
dasshoumik95-ai <dasshoumik95@gmail.com>
2026-06-16 15:34:36 +0530

    Accepted: 1618C - Paint the Array (Submission 379149321)

---
+++ 1618C_379149321_Paint_the_Array.cpp
@@ -0,0 +1,92 @@
+// Codeforces Problem: 1618C - Paint the Array
+// Submission ID: 379149321
+// Language: C++23 (GCC 14-64, msys2)
+
+#include <bits/stdc++.h>
+using namespace std;
+ //    ================== ⚡          ⚡ ==================
+// Author: Shoumik 25CS10034
+// Contest: Contest Name
+// Problem: C_Paint_the_Array
+// Rating: 1100
+// =============================================================
+ #define fast ios::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+ // Loops (0-n), (n-0), (1-n), (n-1)
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define for0(i, n) for(int i = 0; i < n; ++i)          // 0 to n-1
+#define forn(i, n) for(int i = n; i >= 0; --i)         // n down to 0
+#define for1(i, n) for(int i = 1; i <= n; ++i)         // 1 to n
+#define for1n(i, n) for(int i = n; i >= 1; --i)        // n down to 1
+ // Vector Helpers
+#define vecInput(v) for(auto &i : v) cin >> i;
+#define vecOutput(v) for(const auto &i : v) cout << i << ' '; cout << '\n';
+ // Fast I/O Helpers
+#define YES cout << "YES" << '\n';
+#define NO cout << "NO" << '\n';
+ // Debugger (Runs locally with -DLOCAL flag)
+#ifdef LOCAL
+#define deb(x) cerr << #x << " = " << x << endl;
+#else
+#define deb(x)
+#endif
+ const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+ // Safe Modular Arithmetic Helpers
+ll modAdd(ll a, ll b) { return ((a % MOD) + (b % MOD)) % MOD; }
+ll modMul(ll a, ll b) { return ((a % MOD) * (b % MOD)) % MOD; }
+ void solve() {
+    int n ; cin >> n ;
+    vector<ll>a(n) ;
+    vecInput(a) ;
+     ll gcd1 = 0 , gcd2 = 0 ;
+     for0(i,n)
+        if(i%2) //odd
+            gcd1 = __gcd(gcd1,a[i]) ;
+        else
+            gcd2 = __gcd(gcd2,a[i]) ;
+        bool ok = true ;
+    for(int i = 0; i < n; i += 2)
+    {
+        if(a[i] % gcd1 == 0)
+        {
+            ok = false ; break ;
+        }
+    }
+    if(ok)
+    {
+        cout << gcd1 << '\n' ;
+        return ;
+    }
+     ok = true ;
+    for(int i = 1; i < n; i += 2)
+    {
+        if(a[i] % gcd2 == 0)
+        {
+            ok = false ; break ;
+        }
+    }
+    if(ok)
+    {
+        cout << gcd2 << '\n' ;
+    }
+    else cout << 0 << '\n' ;
+}
+ int main() {
+    fast;
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
+// 7 7 3 6 9 2 17
+// 1 2
+ //  14 21 28 63 2 126
+//  21 
