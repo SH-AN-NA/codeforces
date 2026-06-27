074e7ac4f89b8f7923a3baed1855b329ec4d48fa
dasshoumik95-ai <dasshoumik95@gmail.com>
2026-06-27 15:32:59 +0530

    Accepted: 2240B - AI Finds Nothing Here (Submission 380348168)

---
+++ 2240B_380348168_AI_Finds_Nothing_Here.cpp
@@ -0,0 +1,78 @@
+// Codeforces Problem: 2240B - AI Finds Nothing Here
+// Submission ID: 380348168
+// Language: C++23 (GCC 14-64, msys2)
+
+#include <bits/stdc++.h>
+using namespace std;
+ //    ================== ⚡          ⚡ ==================
+// Author: Shoumik 25CS10034
+// Contest: Contest Name
+// Problem: B_AI_Finds_Nothing_Here
+// Rating: Rating
+// =============================================================
+ #define fast ios::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+ // Loops (0-n), (n-0), (1-n), (n-1)
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define for0(i, n) for(int i = 0; i < n; ++i)          // 0 to n-1
+#define forn(i, n) for(int i = n; i >= 0; --i)         // n down to 0
+#define for1(i, n) for(int i = 1; i <= n; ++i)         // 1 to n
+#define for1n(i, n) for(int i = n; i >= 1; --i)        // n down to 1
+ // Vector Helpers
+#define vecInput(v) for(auto &i : v) cin >> i;
+#define vecOutput(v) for(const auto &i : v) cout << i << ' '; cout << '\n';
+ // Fast I/O Helpers
+#define YES cout << "YES" << '\n';
+#define NO cout << "NO" << '\n';
+ // Debugger (Runs locally with -DLOCAL flag)
+#ifdef LOCAL
+#define deb(x) cerr << #x << " = " << x << endl;
+#else
+#define deb(x)
+#endif
+ const int MOD =  998244353 ;
+const ll INF = 1e18;
+ // Safe Modular Arithmetic Helpers
+ll modAdd(ll a, ll b) { return ((a % MOD) + (b % MOD)) % MOD; }
+ll modMul(ll a, ll b) { return ((a % MOD) * (b % MOD)) % MOD; }
+// Modular exponentiation
+ll power(ll base, ll exp) {
+    ll res = 1;
+    base %= MOD;
+    while (exp > 0) {
+        if (exp % 2 == 1) res = (res * base) % MOD;
+        base = (base * base) % MOD;
+        exp /= 2;
+    }
+    return res;
+}
+void solve() {
+    ll n , m , r , c ;
+    cin >> n >> m >> r >> c;
+     ll M = MOD - 1 ;
+     ll total = ((n%M) * (m%M)) % (M) ;
+     ll row = max(0LL , n - r +1) ;
+    ll col = max(0LL , m - c + 1) ;
+     ll sq = ((row%M) * (col%M)) % (M) ;
+     cout << power(2 ,(total - sq + M) % (M))  << '\n' ;
+}
+ int main() {
+    fast;
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
+ // 4 6 2 2
+// . . . . . .
+// . . . . . .
+// . . . . . .
+// . . . . . .
+ // 2 2 -- 4 same -- 2 /2 same * 2
