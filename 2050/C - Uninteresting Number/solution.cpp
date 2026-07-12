3308ccbd277e4606d3d3bf21f20724204484c640
Shoumik Das <dasshoumik95@gmail.com>
2026-07-13 01:41:28 +0530

    Add Uninteresting Number [C] from Codeforces

---
+++ 2050/C - Uninteresting Number/solution.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//    ================== ⚡          ⚡ ==================
+// Author: Shoumik 25CS10034
+// Contest: Contest Name
+// Problem: C_Uninteresting_Number
+// Rating: Rating
+// =============================================================
+ 
+#define fast ios::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+ 
+// Loops (0-n), (n-0), (1-n), (n-1)
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define for0(i, n) for(int i = 0; i <= n; ++i)          // 0 to n-1
+#define forn(i, n) for(int i = n; i >= 0; --i)         // n down to 0
+#define for1(i, n) for(int i = 1; i <= n; ++i)         // 1 to n
+#define for1n(i, n) for(int i = n; i >= 1; --i)        // n down to 1
+ 
+// Vector Helpers
+#define vecInput(v) for(auto &i : v) cin >> i;
+#define vecOutput(v) for(const auto &i : v) cout << i << ' '; cout << '
+';
+ 
+// Fast I/O Helpers
+#define YES cout << "YES" << '
+';
+#define NO cout << "NO" << '
+';
+ 
+// Debugger (Runs locally with -DLOCAL flag)
+#ifdef LOCAL
+#define deb(x) cerr << #x << " = " << x << endl;
+#else
+#define deb(x)
+#endif
+ 
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+ 
+// Safe Modular Arithmetic Helpers
+ll modAdd(ll a, ll b) { return ((a % MOD) + (b % MOD)) % MOD; }
+ll modMul(ll a, ll b) { return ((a % MOD) * (b % MOD)) % MOD; }
+ 
+void solve() {
+    string n ; cin >> n ;
+ 
+    int cnt2 = 0 , cnt3 = 0 ;
+    ll sum = 0 ;
+    for (char c : n) {
+        int digit = c - '0';
+        sum += digit;
+        if (digit == 2) cnt2++;
+        if (digit == 3) cnt3++;
+    }
+    for(int i = 0 ; i <= min(9,cnt3) ; i++) {
+        for0(j,(min(9,cnt2))) {
+            if((sum + 6*i + j*2 )%9 == 0) {
+                YES 
+                return ;
+            }
+        }
+    }
+    NO ;
+    
+}
+ 
+int main() {
+    fast;
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
