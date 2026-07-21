39eeec268530f5ea6eb37d2dc938cf9b86d36b3b
Shoumik Das <dasshoumik95@gmail.com>
2026-07-21 23:44:38 +0530

    Add Jeff and Digits [A] from Codeforces

---
+++ 352/A - Jeff and Digits/solution.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//    ================== ⚡          ⚡ ==================
+// Author: Shoumik 25CS10034
+// Contest: Contest Name
+// Problem: A_Jeff_and_Digits
+// Rating: Rating
+// =============================================================
+ 
+#define fast ios::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+ 
+// Loops (0-n), (n-0), (1-n), (n-1)
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define for0(i, n) for(int i = 0; i < n; ++i)          // 0 to n-1
+#define forn(i, n) for(int i = n; i >= 0; --i)         // n down to 0
+#define for1(i, n) for(int i = 1; i <= n; ++i)         // 1 to n
+#define for1n(i, n) for(int i = n; i >= 1; --i)        // n down to 1
+ 
+// Vector Helpers
+#define vecInput(v) for(auto &i : v) cin >> i;
+#define vecOutput(v) for(const auto &i : v) cout << i << ' '; cout << '
+';
+ 
+// Fast I/O Helpers
+#define YES cout << "YES" << '
+';
+#define NO cout << "NO" << '
+';
+ 
+// Debugger (Runs locally with -DLOCAL flag)
+#ifdef LOCAL
+#define deb(x) cerr << #x << " = " << x << endl;
+#else
+#define deb(x)
+#endif
+ 
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+ 
+// Safe Modular Arithmetic Helpers
+ll modAdd(ll a, ll b) { return ((a % MOD) + (b % MOD)) % MOD; }
+ll modMul(ll a, ll b) { return ((a % MOD) * (b % MOD)) % MOD; }
+ 
+void solve() {
+    int n ; cin >> n ;
+    vector<int> a(n) ;
+    
+    int cnt5 = 0 , cnt0 = 0 ;
+ 
+    for0(i,n) {
+        cin >> a[i] ;
+ 
+        if(a[i]==5)
+            cnt5++ ;
+        else
+            cnt0++ ;
+ 
+    }
+ 
+    if (cnt0 == 0) {
+        cout << -1 << '
+';
+        return;
+    }
+    
+    if( cnt5 < 9) {
+        cout << 0 << '
+' ;
+        return ;
+    }
+ 
+    string t = "" ;
+ 
+    for(int i = 0 ; i < cnt5/9 ; i++) {
+        t += "555555555" ;
+    }
+ 
+    for(int i = 0 ; i < cnt0 ; i++) {
+        t += "0" ;
+    }
+ 
+    cout << t ; 
+}
+ 
+int main() {
+    fast;
+    int t = 1;
+    // cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
