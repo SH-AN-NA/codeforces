8a9f98660e397fee2fb645378eeb540202e2e15c
Shoumik Das <dasshoumik95@gmail.com>
2026-07-25 00:46:04 +0530

    Add Spring [C] from Codeforces

--- 2204/C - Spring/solution.cpp
+++ 2204/C - Spring/solution.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-#include <numeric>
 using namespace std;
  
 //    ================== ⚡          ⚡ ==================
@@ -48,49 +47,28 @@ const ll INF = 1e18;
 // Safe Modular Arithmetic Helpers
 ll modAdd(ll a, ll b) { return ((a % MOD) + (b % MOD)) % MOD; }
 ll modMul(ll a, ll b) { return ((a % MOD) * (b % MOD)) % MOD; }
-// Helper function to safely calculate LCM without overflow
-ll get_lcm(ll a, ll b, ll limit) {
-    if (a == 0 || b == 0) return limit + 1;
-    ll g = std::__gcd(a, b);
  
-    if (a / g > limit / b) return limit + 1; 
-    return (a / g) * b;
+ll lcm(ll a , ll b) {
+    return (a / __gcd(a, b)) * b;
 }
  
 void solve() {
-    ll a, b, c, m;
-    cin >> a >> b >> c >> m;
+    ll a , b , c , m ;
+    cin >> a >> b >> c >> m ;
  
-    ll lcm_ab = get_lcm(a, b, m);
-    ll lcm_bc = get_lcm(b, c, m);
-    ll lcm_ac = get_lcm(a, c, m);
-    ll lcm_abc = get_lcm(lcm_ab, c, m);
- 
-    ll count_a   = m / a;
-    ll count_b   = m / b;
-    ll count_c   = m / c;
+    ll sol = m/(a) * 6 - m/(lcm(a,b)) * 3 - m/(lcm(a,c)) * 3 + m/(lcm(c,lcm(a,b)))*2 ;
+        cout << sol << " " ;
     
-    ll count_ab  = m / lcm_ab;
-    ll count_bc  = m / lcm_bc;
-    ll count_ac  = m / lcm_ac;
-    
-    ll count_abc = m / lcm_abc;
+    sol = m/(b) * 6 - m/(lcm(a,b)) * 3 - m/(lcm(b,c)) * 3 + m/(lcm(c,lcm(a,b)))*2 ;
+        cout << sol << " " ;
  
-    ll only_a  = count_a - count_ab - count_ac + count_abc;
-    ll only_b  = count_b - count_ab - count_bc + count_abc;
-    ll only_c  = count_c - count_ac - count_bc + count_abc;
+    sol = m/(c) * 6 - m/(lcm(c,b)) * 3 - m/(lcm(a,c)) * 3 + m/(lcm(c,lcm(a,b)))*2 ;
+        cout << sol << " " ;
  
-    ll ab_only = count_ab - count_abc;
-    ll bc_only = count_bc - count_abc;
-    ll ac_only = count_ac - count_abc;
- 
-    ll score_a = 6 * only_a + 3 * ab_only + 3 * ac_only + 2 * count_abc;
-    ll score_b = 6 * only_b + 3 * ab_only + 3 * bc_only + 2 * count_abc;
-    ll score_c = 6 * only_c + 3 * ac_only + 3 * bc_only + 2 * count_abc;
- 
-    cout << score_a << " " << score_b << " " << score_c << '
-';
+    cout << '
+' ;
 }
+ 
 int main() {
     fast;
     int t = 1;
