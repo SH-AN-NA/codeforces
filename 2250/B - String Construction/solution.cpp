d161926c701e5925c0f51b985e9b4b89d99c7f57
Shoumik Das <dasshoumik95@gmail.com>
2026-07-27 18:57:08 +0530

    Add String Construction [B] from Codeforces

---
+++ 2250/B - String Construction/solution.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//    ================== ⚡          ⚡ ==================
+// Author: Shoumik 25CS10034
+// Contest: Contest Name
+// Problem: B_String_Construction
+// Rating: Rating
+// =============================================================
+ 
+#define fast ios::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define ld long double
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+ 
+// Loops (0-n), (n-0), (1-n), (n-1)
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define for0(i, n) for(int i = 0; i < n; ++i)          // 0 to n-1
+#define forn(i, n) for(int i = n; i >= 0; --i)         // n down to 0
+#define for1(i, n) for(int i = 1; i <= n; ++i)         // 1 to n
+#define for1n(i, n) for(int i = n; i >= 1; --i)        // n down to 1
+ 
+// Vector Helpers
+#define vecInput(v) for(auto &i : v) cin >> i;
+#define vecOutput(v) for(const auto &i : v) cout << i << ' '; cout << '
+';
+ 
+// Fast I/O Helpers
+#define YES cout << "YES" << '
+';
+#define NO cout << "NO" << '
+';
+ 
+// Debugger (Runs locally with -DLOCAL flag)
+#ifdef LOCAL
+#define deb(x) cerr << #x << " = " << x << endl;
+#else
+#define deb(x)
+#endif
+ 
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+ 
+// Safe Modular Arithmetic Helpers
+ll modAdd(ll a, ll b) { return ((a % MOD) + (b % MOD)) % MOD; }
+ll modMul(ll a, ll b) { return ((a % MOD) * (b % MOD)) % MOD; }
+ 
+void solve() {
+    ll n , k ;
+    cin >> n >> k ;
+ 
+    if(n-2 < k) {
+        cout << -1 << '
+' ;
+        return ;
+    }
+    string s = "" ;
+ 
+    
+    for(int i = 0 ; i < k/2 ; i++) {
+        s += "0" ;
+    }
+    s += "0" ;
+    for(int i = k/2 ; i < k ; i++) {
+        s += "1" ;
+    }
+    s += "1" ;
+ 
+    int sze = s.size() ; // k+2
+ 
+    bool evenstart = 0 ;
+    if(k%2) 
+        evenstart = 1 ;
+    for(int i = sze ; i < n ; i++) {
+        if(evenstart) {
+            if(i%2==0)
+                s += "1" ;
+            else
+                s += "0" ;
+        }
+        else {
+            if(i%2==0)
+                s += "0" ;
+            else
+                s += "1" ;
+        }
+    
+    }
+ 
+    cout << s << '
+' ;
+    
+}
+ 
+int main() {
+    fast;
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
+ 
+// 15 9
+// 0s 8 1s 7
+// 7 + 6 = 13
+ 
+//14 12
+ 
+// 13
